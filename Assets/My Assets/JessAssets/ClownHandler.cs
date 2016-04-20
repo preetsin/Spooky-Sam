747859a892dde22773862fbb6bdb67f2d1b6c872
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-04-21 00:22:23 +1000

    started hooking up controller to clown and navmesh

---
+++ Assets/My Assets/JessAssets/ClownHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClownHandler : MonoBehaviour {
+	public Transform[] targetDestinations;
+	NavMeshAgent agent;
+	public Animator animator;
+
+	int targetId;
+
+	void Start () {
+		
+		targetId = 0;
+		agent = GetComponent<NavMeshAgent> ();
+		agent.destination = targetDestinations[targetId].position;
+		animator.SetBool ("isWalking", true);
+	}
+
+	void Update () {
+
+		if (agent.destination == agent.transform.position) {
+
+			targetId++;
+
+			if (targetId >= targetDestinations.Length) {
+				targetId = 0;
+			}
+
+			agent.destination = targetDestinations[targetId].position;
+
+
+		}
+	
+	}
+}
