9fea7145ceaad5c8e28a9a212cdcdda9c41a96c4
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-04-21 00:02:03 +1000

    added clown zombies and the script that iw as using to protoype them outside of this project

---
+++ Assets/My Assets/JessAssets/clownWalk.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clownWalk : MonoBehaviour {
+
+	public GameObject[] destinations;
+	int currDestinationId;
+	NavMeshAgent clown;
+	GameObject currObj;
+
+
+
+	void Start () {
+
+		currDestinationId = 0;
+		clown = GetComponent<NavMeshAgent>();
+
+		GameObject currObj = destinations [currDestinationId];
+		clown.destination = currObj.transform.position; 
+
+		GetComponent<Animator> ().SetBool ("isWalking", true);
+
+		currObj = destinations[currDestinationId];
+
+	}
+
+
+
+
+	void Update () {
+
+
+//		clown.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+		if (clown.destination == clown.transform.position) {
+
+			GetComponent<Animator> ().SetBool ("isWalking", false);
+
+			currDestinationId++;
+
+
+			if (currDestinationId < destinations.Length) {
+				
+				currObj = destinations [currDestinationId];
+
+			} else {
+				
+				currDestinationId = 0;
+
+			}
+
+			currObj = destinations [currDestinationId];
+			clown.destination = currObj.transform.position; 
+			GetComponent<Animator> ().SetBool ("isWalking", true);
+		
+		}
+			
+	}
+}
