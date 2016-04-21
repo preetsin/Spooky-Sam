500e553f4a684986d1520cf9f870cfed9c748129
Admin <admin@Admins-MacBook-Pro.local>
2016-04-21 15:52:20 +1000

    Added DoorLever to the graveyard scene
    Added Scripts folder to my assets

---
+++ Assets/My Assets/Scripts/GraveyardScripts/DoorLeverScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorLeverScript : MonoBehaviour {
+
+	TextMesh leverText;
+
+	void Start () {
+		leverText = GameObject.Find ("LeverText").GetComponent<TextMesh> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		leverText.text = "Press Some Button";
+		Debug.Log ("Object Entered Trigger");
+	}
+
+	void OnTriggerExit(Collider other){
+		leverText.text = "";
+		Debug.Log ("Object Exit Trigger");
+	}
+}
