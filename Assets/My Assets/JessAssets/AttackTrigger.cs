ae03555e8869d3facaa90f4080cafe35b8c369e9
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-04-22 00:54:32 +1000

    made so that when the player enters the huddle part of the maze scene, the clowns all get up form around the fire, turn to face the player ready to fight, and fight music starts to play.

---
+++ Assets/My Assets/JessAssets/AttackTrigger.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackTrigger : MonoBehaviour {
+
+	bool attackMode = false;
+	int currentEnemyId = 0;
+	public GameObject[] enemyObjects;
+
+	GameObject player;
+
+	AudioSource backgroundMusicSource;
+	AudioClip backgroundMusic;
+
+
+
+
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+
+			player = col.gameObject;
+
+			backgroundMusicSource.Play ();
+
+			attackMode = true;
+
+			foreach (GameObject enemyObj in enemyObjects) {
+
+				Animator enemyAnimator = enemyObj.GetComponentInChildren<Animator> ();
+
+				// enemies all stand up to face player
+				enemyAnimator.SetBool ("isSitting", false);
+				enemyAnimator.SetBool ("isKneeling", false);
+				enemyAnimator.SetBool ("isWalking", false);
+
+				enemyObj.transform.LookAt (player.transform);
+
+			}
+		}
+	}
+
+
+	void Start () {
+
+		backgroundMusicSource = gameObject.GetComponent<AudioSource> ();
+
+	
+	}
+	
+	void Update () {
+
+		if (attackMode) {
+
+
+			//clown get ready to fight... each showing off their signature move
+			//clown attack one by one
+		}
+	
+	}
+}
