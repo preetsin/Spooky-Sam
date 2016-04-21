b1162ee802422eefcc99e21e9acbbdc1f9c0914e
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-04-21 18:28:23 +1000

    added trigger at entrance to huddled clowns so the clowns known when to start doing stuff

---
+++ Assets/AttackTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackTrigger : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("Player is close by");
+		} else {
+			Debug.Log ("not sure who this is");
+		}
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
