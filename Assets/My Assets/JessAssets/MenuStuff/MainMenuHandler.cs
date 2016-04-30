5b7b9ca5b077d4ba60845b151ae81c5b4338deaf
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-04-30 16:13:17 +1000

    started working on UI elements. Have an opening title screen, which leads into the main menu. at the moment the main menu has a play button which starts the first level 'house level'

---
+++ Assets/My Assets/JessAssets/MenuStuff/MainMenuHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuHandler : MonoBehaviour {
+
+	public void buttonWasPressed(string bttnName) {
+
+		switch(bttnName) {
+
+		case "playBttn":
+			Debug.Log ("PLAY the GAME, ya psycho!");	
+			SceneManager.LoadScene ("houseScene");
+			break;
+		default:
+			Debug.Log ("Error: no button detected");
+			break;
+
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/My Assets/JessAssets/OldChest/OpeningTitleAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class OpeningTitleAnimation : MonoBehaviour {
+
+
+	private RawImage openingTitleImage;
+	private int timer;
+	private float alphaStart = 1.0f;
+	private float alphaFinish = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+		openingTitleImage = GetComponent<RawImage> ();
+		timer = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer++;
+
+		 if (timer == 120) {
+			openingTitleImage.CrossFadeColor (Color.black, 5.0f, false, false);
+		} else if (timer == 500) {
+			SceneManager.LoadScene ("MainMenu");
+		}
+
+
+
+
+	
+	}
+}
