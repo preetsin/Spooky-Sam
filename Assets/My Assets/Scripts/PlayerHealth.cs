5e9c5c1b1bd12979f735ab1f34ecdbe8431f0270
Adam Rehn <adam.rehn@my.jcu.edu.au>
2016-05-06 16:26:59 +1000

    Un-break everything.

---
+++ Assets/My Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+    public GameObject canvas;
+	public Slider healthBar;
+	private int timer;
+
+
+	void Awake () {
+
+		healthBar.value = Prefs.playerHealth;
+		timer = 0;
+	}
+    
+
+    void accumulatehealth()
+    {
+		Prefs.playerHealth += 10;
+
+		if (Prefs.playerHealth > 100) {
+			
+			Prefs.playerHealth = 100;
+
+		}
+		healthBar.value = Prefs.playerHealth;
+
+
+    }
+
+    void Update () {
+
+		healthBar.value = Prefs.playerHealth;
+		//this line is necessary to make sure any changes to the 
+		//health made outside of this script are shown on the healthbar
+
+		timer++;
+
+		if (timer == 600) { // seconds
+			timer = 0;
+			accumulatehealth ();
+		}
+
+	}
+}
---
+++ Assets/My Assets/Scripts/PlayerWeapons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerWeapons : MonoBehaviour {
+
+    public List<int> weapons;
+
+    void Awake() {
+        weapons = new List<int>();
+    }
+
+    public void setWeapons(int i) {
+        weapons.Add(i);
+		Debug.Log ("Weapon Added");
+    }
+}
