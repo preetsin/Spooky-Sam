d16d61bcf21b690d4a36e84f7b2bebfc6b7aca3c
Parminder Singh <preetzdomain@gmail.com>
2016-05-08 22:40:05 +1000

    Added more alerts and fixed in script to display once only.

--- Assets/My Assets/Scripts/GraveyardScripts/AlertScript.cs
+++ Assets/My Assets/Scripts/GraveyardScripts/AlertScript.cs
@@ -5,22 +5,36 @@ public class AlertScript : MonoBehaviour {
 
     AlertViewHandler alert;
 
+    bool exitDoorMsgDisplayed;
+    bool doorLeverCageMsgDisplayed;
+    bool outsideDoorInstructionDisplayed;
+
     void Start()
     {
         alert = FindObjectOfType<AlertViewHandler>();
+        exitDoorMsgDisplayed = false;
+        doorLeverCageMsgDisplayed = false;
+        outsideDoorInstructionDisplayed = false;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && this.gameObject.name == "ExitDoorLeverCageInstructionCollider")
+        if (other.CompareTag("Player") && this.gameObject.name == "ExitDoorLeverCageInstructionCollider" && !doorLeverCageMsgDisplayed)
         {
             alert.showAlert("Find gun to shootdown door and get access to Exit Door Lever.");
+            doorLeverCageMsgDisplayed = true;
         }
         
-        if (other.CompareTag("Player") && this.gameObject.name == "ExitDoorInstructionCollider")
+        if (other.CompareTag("Player") && this.gameObject.name == "ExitDoorInstructionCollider" && !exitDoorMsgDisplayed)
         {
-            alert.showAlert("Toggle Door Lever to Open Exit door .");
+            alert.showAlert("Toggle Door Lever to Open Exit door.");
+            exitDoorMsgDisplayed = true;
         }
 
+        if (other.CompareTag("Player") && this.gameObject.name == "OutsideDoorInstructionCollider" && !outsideDoorInstructionDisplayed)
+        {
+            alert.showAlert("Door is locked. Will need to jump over the fence");
+            outsideDoorInstructionDisplayed = true;
+        }
     }
 }
