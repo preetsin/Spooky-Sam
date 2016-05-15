c02f7c9a0472fbc65456e82c8f39ba30f2882cad
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-05-15 21:38:16 +1000

    gate added to end of maze, maze is reduced as was too complex, exit gate only opens if the keys have been collected. when the player leaves through the gate then they transition to next scene

---
+++ Assets/mazeEndHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class mazeEndHandler : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			SceneManager.LoadScene ("graveScene");
+		}
+	}
+}
---
+++ Assets/mazeExitHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mazeExitHandler : MonoBehaviour {
+	public GameObject exitGate;
+	private bool gateCanOpen;
+
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			if (Prefs.hasKeys) {
+				gateCanOpen = true;
+			} else {
+				AlertViewHandler alert = FindObjectOfType<AlertViewHandler> ();
+				alert.showAlert ("Nice try... but i DID try to tell you that you needed those keys.");
+			}
+		}
+
+	}
+
+	void Start () {
+		gateCanOpen = false;
+	}
+
+	void Update() {
+		Quaternion newQuat = new Quaternion (0.0f, -0.1f, 0.0f, 1.0f);
+
+		if (gateCanOpen) {
+			if (exitGate.transform.rotation != newQuat) {
+				exitGate.transform.Rotate (Time.deltaTime * 0, -0.7f, 0);
+			}
+		}
+	}
+}
