07abbbd0754948d43377695bf157144143c22522
advocaat <adam.nicholls@my.jcu.edu.au>
2016-05-16 00:18:41 +1000

    fixed bug in damage script/ moved update out of prefs

--- Assets/My Assets/JessAssets/MenuStuff/Prefs.cs
+++ Assets/My Assets/JessAssets/MenuStuff/Prefs.cs
@@ -2,15 +2,8 @@
 using System.Collections;
 
 public class Prefs : MonoBehaviour {
-    public static int lastHealth = 50;
+   
     public static int playerHealth = 50;
-
-    void Start() {
-        lastHealth = playerHealth;
-    }
-
-    void Update() {
-        lastHealth = playerHealth;
-    }
+    public static bool hasKeys = false;
 
 }
--- Assets/My Assets/Scripts/PlayerHealth.cs
+++ Assets/My Assets/Scripts/PlayerHealth.cs
@@ -6,12 +6,14 @@ public class PlayerHealth : MonoBehaviour {
     public GameObject canvas;
 	public Slider healthBar;
 	private int timer;
-    public bool healthIncremented;
+    public static int lastHealth;
 
-	void Awake () {
 
+    void Awake () {
+        lastHealth = Prefs.playerHealth;
 		healthBar.value = Prefs.playerHealth;
 		timer = 0;
+        StartCoroutine(healthUpdate());
 	}
     
 
@@ -20,45 +22,50 @@ public class PlayerHealth : MonoBehaviour {
 
 
 		Prefs.playerHealth += 10;
-        canvas.GetComponent<RawImage>().enabled = true;
+        canvas.GetComponent<RawImage>().enabled = false;
         if (Prefs.playerHealth > 100) {
-			
 			Prefs.playerHealth = 100;
-
 		}
 		healthBar.value = Prefs.playerHealth;
-        healthIncremented = true;
-
     }
 
     void Update () {
 
 		healthBar.value = Prefs.playerHealth;
-
+       
 		if (Prefs.playerHealth <= 10) {
             canvas.GetComponent<RawImage>().enabled = true;
 		} else {
             StartCoroutine(disableImage());
+            lastHealth = Prefs.playerHealth;
         }
+       
+      // timer++;
 
-		timer++;
-
-		if (timer == 600) { // seconds
-			timer = 0;
-			accumulatehealth ();
-		}
-
-
-
+		//if (timer == 600) { // seconds
+		//	timer = 0;
+		//	accumulatehealth ();
+		//}
 	}
+
+
+    IEnumerator healthUpdate() {
+        while (true) {
+            yield return new WaitForSeconds(8);
+            accumulatehealth();
+        }
+    }
+
+
     IEnumerator disableImage()
     {
-        while (Prefs.lastHealth > Prefs.playerHealth)
+        while (lastHealth > Prefs.playerHealth)
         {
+            
             canvas.GetComponent<RawImage>().enabled = true;
             yield return new WaitForSeconds(0.15f);
             canvas.GetComponent<RawImage>().enabled = false;
-            Prefs.lastHealth = Prefs.playerHealth;
+            
         }
 
     }
