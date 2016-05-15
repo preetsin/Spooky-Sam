f479f7f1672e0dec8716d7c503dfc6bd25f4588d
advocaat <adam.nicholls@my.jcu.edu.au>
2016-05-15 21:29:06 +1000

    red overlay when recieve hurt

--- Assets/My Assets/JessAssets/MenuStuff/Prefs.cs
+++ Assets/My Assets/JessAssets/MenuStuff/Prefs.cs
@@ -2,11 +2,15 @@
 using System.Collections;
 
 public class Prefs : MonoBehaviour {
+    public static int lastHealth = 50;
+    public static int playerHealth = 50;
 
-	public static int playerHealth = 50;
+    void Start() {
+        lastHealth = playerHealth;
+    }
 
-
-
-
+    void Update() {
+        lastHealth = playerHealth;
+    }
 
 }
