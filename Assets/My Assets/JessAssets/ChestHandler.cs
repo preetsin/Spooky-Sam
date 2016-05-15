359770a7b2ea22163f047a297acb1dc834d2aea7
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-05-15 16:20:35 +1000

    Chest in culdesac now opens to set angle with animation upon collision... will further change this so only opens when clowns are dead

---
+++ Assets/My Assets/JessAssets/ChestHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestHandler : MonoBehaviour {
+
+	public GameObject lid;
+	private bool lidCanOpen;
+
+	// Use this for initialization
+	void Start () {
+		lidCanOpen = false;
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			lidCanOpen = true;
+		}
+
+		//		AlertViewHandler alert = FindObjectOfType<AlertViewHandler> ();
+		//		alert.showAlert ("You walked into the fire... what a loser");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion newQuat = new Quaternion (-0.6f, 0.8f, -0.1f, 0.1f);
+		if (lidCanOpen) {
+			if (lid.transform.rotation != newQuat) {
+				lid.transform.Rotate (Time.deltaTime * 0, 0, -1);
+				Debug.Log (lid.transform.rotation);
+			} else {
+				lidCanOpen = false;
+			}
+		}
+	
+	}
+}
