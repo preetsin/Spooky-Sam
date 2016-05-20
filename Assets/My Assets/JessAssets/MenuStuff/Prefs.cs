2a41f0f4b34b76594d023941ded67e6470dbae52
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-05-20 19:28:06 +1000

    added settings for music

--- Assets/My Assets/JessAssets/MenuStuff/Prefs.cs
+++ Assets/My Assets/JessAssets/MenuStuff/Prefs.cs
@@ -6,4 +6,7 @@ public class Prefs : MonoBehaviour {
     public static int playerHealth = 50;
     public static bool hasKeys = false;
 
+	public static bool musicEnabled = true;
+	public static bool sfxEnabled = true;
+
 }
---
+++ Assets/My Assets/JessAssets/MenuStuff/SettingsHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SettingsHandler : MonoBehaviour {
+
+	public Toggle musicToggle;
+	public Toggle sfxToggle;
+
+
+	public void updateSettings() {
+		if (musicToggle.isOn) {
+			Prefs.musicEnabled = true;
+		} else {
+			Prefs.musicEnabled = false;
+		}
+
+		if (sfxToggle.isOn) {
+			Prefs.sfxEnabled = true;
+		} else {
+			Prefs.sfxEnabled = false;
+		}
+
+		Debug.Log ("Music : " + Prefs.musicEnabled);
+		Debug.Log ("SFX : " + Prefs.sfxEnabled);
+
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
