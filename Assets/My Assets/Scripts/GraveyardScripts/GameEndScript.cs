3250732410cdb2d9a4c680db520f7f5c782defe8
Parminder Singh <preetzdomain@gmail.com>
2016-05-22 01:37:05 +1000

    Added game ending. Once player kill all AIs and walk out exit door, another camera will kick in and display text while playing animation.

---
+++ Assets/My Assets/Scripts/GraveyardScripts/GameEndScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEndScript : MonoBehaviour {
+
+    GameObject gameEndCamera;
+    Camera mainCam;
+    Camera gameEndCam;
+    Animation gameEndCamAnimation;
+    AudioListener gameEndCamAudioListener;
+    GraveyardDataManager graveyardDataManager;
+    Animator theHudAnimator;
+    AudioSource audio;
+
+
+    void Start () {
+        gameEndCamera = GameObject.Find("GameCompletionCam");
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        gameEndCam = gameEndCamera.GetComponent<Camera>();
+        gameEndCamAudioListener = gameEndCam.GetComponent<AudioListener>();
+        gameEndCamAnimation = gameEndCamera.GetComponent<Animation>();
+        graveyardDataManager = GraveyardDataManager.GetInstance();
+        theHudAnimator = GameObject.Find("TheHUD").GetComponent<Animator>();
+        audio = GameObject.FindGameObjectWithTag("BackgroundSound").GetComponent<AudioSource>();
+
+        gameEndCamAudioListener.enabled = false;
+        gameEndCam.enabled = false;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mainCam.enabled = false;
+            gameEndCam.enabled = true;
+            gameEndCamAnimation.Play();
+            graveyardDataManager.Shooting = false;
+            theHudAnimator.SetTrigger("GameEnded");
+            audio.enabled = false;
+        }
+    }
+
+}
