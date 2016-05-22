ac9afa0c2156c9caf2796b785fdb13dc7a61e3b0
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-05-22 16:08:03 +1000

    added level end alerts

--- Assets/My Assets/Scripts/winHouseStage.cs
+++ Assets/My Assets/Scripts/winHouseStage.cs
@@ -3,11 +3,30 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class winHouseStage : MonoBehaviour {
+	AlertViewHandler alerts;
+	bool isWaiting = true;
 
+	void Start(){
+		alerts = FindObjectOfType<AlertViewHandler> ();
+
+
+	}
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Player") {
+			StartCoroutine (nextStage ());
+		}
+	
+	
+	}
+
+	IEnumerator nextStage() {
+		while (isWaiting) {
+
+			alerts.showAlert ("Level 1 Complete");
+			yield return new WaitForSeconds (0.5f);
+			alerts.dismissAlert ();
 			SceneManager.LoadScene ("mazeScene");
-		
+			isWaiting = false;
 		}
 	
 	}
--- Assets/mazeEndHandler.cs
+++ Assets/mazeEndHandler.cs
@@ -3,10 +3,29 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class mazeEndHandler : MonoBehaviour {
+	AlertViewHandler alerts;
+	bool isWaiting = true;
+
+
+	void Start(){
+		alerts = FindObjectOfType<AlertViewHandler> ();
+	}
 
 	void OnTriggerEnter(Collider col) {
 		if (col.tag == "Player") {
-			SceneManager.LoadScene ("graveScene");
+			StartCoroutine (nextStage ());
 		}
+	}
+
+	IEnumerator nextStage() {
+		while (isWaiting) {
+
+			alerts.showAlert ("Level 2 Complete");
+			yield return new WaitForSeconds (0.5f);
+			alerts.dismissAlert ();
+			SceneManager.LoadScene ("graveScene");
+			isWaiting = false;
+		}
+
 	}
 }
