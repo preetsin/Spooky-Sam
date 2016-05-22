1943984f667b24a35294ee2d9e6390a75962283b
codeninja88 <jessica.mckenzie@my.jcu.edu.au>
2016-05-22 14:51:56 +1000

    separated the death logic from the graveyard specific death

--- Assets/My Assets/Scripts/GraveyardScripts/PlayerDies.cs
+++ Assets/My Assets/Scripts/GraveyardScripts/PlayerDies.cs
@@ -7,12 +7,10 @@ public class PlayerDies : MonoBehaviour
 
     GraveyardController graveyardController;
     GraveyardDataManager graveyardDataManager;
-    Animator animator;
     bool isDead;
 
     void Start()
     {
-        animator = GetComponent<Animator>();
         isDead = false;
         if (SceneManager.GetActiveScene().name.Equals("graveScene"))
         {
@@ -26,15 +24,11 @@ public class PlayerDies : MonoBehaviour
 
         if (Prefs.playerHealth <= 0 && !isDead)
         {
-            GetComponent<InputScript>().enabled = false;
-            animator.SetTrigger("isDead");
-            isDead = true;
 
             if (SceneManager.GetActiveScene().name.Equals("graveScene"))
             {
                 graveyardDataManager.Shooting = false;
                 GameObject.FindGameObjectWithTag("BackgroundSound").GetComponent<AudioSource>().enabled = false;
-                //graveyardController.EnableGunShooting(false);
                 GetComponent<AudioSource>().enabled = true;
             }   
         }
